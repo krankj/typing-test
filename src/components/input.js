15a28de177fe2ff3be8d447b34240aa96c87daa9
Sudarshan K J <kjsudi@gmail.com>
2020-10-28 21:29:25 +0530

    Basic word detection functionality is complete

--- src/components/input.js
+++ src/components/input.js
@@ -1,17 +1,44 @@
 import React from "react";
 import styles from "./input.module.css";
+const inputArray = [];
+const fetchedString = "My name is Sudarshan and I am coming here";
+const textArray = fetchedString.split(" ");
 
 const InputField = () => {
+  const [correctWords, setCorrectWords] = React.useState(0);
   const [inputValue, setInputValue] = React.useState("");
+
+  const handleInputChange = (e) => {
+    let enteredValue = e.target.value;
+    setInputValue(enteredValue);
+    if (enteredValue === " ") {
+      setInputValue("");
+    }
+  };
+
+  const detectSpacePresent = (e) => {
+    if (e.keyCode === 32) {
+      if (inputValue !== "") {
+        inputArray.push(inputValue);
+        console.log(inputArray);
+        if (inputArray[correctWords] === textArray[correctWords]) {
+          setCorrectWords((prevCount) => prevCount + 1);
+        }
+      }
+      setInputValue("");
+    }
+  };
   return (
     <div>
+      <p>{fetchedString}</p>
       <input
         className={styles.inputField}
         type="text"
         value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        onChange={handleInputChange}
+        onKeyDown={detectSpacePresent}
       />
-      <p>Entered value: {inputValue} </p>
+      <p>Correct words: {correctWords}</p>
     </div>
   );
 };
