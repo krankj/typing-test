817f682fb5b74ff1b52b7a287262587a8fea980e
Sudarshan K J <kjsudi@gmail.com>
2021-01-13 21:41:39 -0800

    Converted dropdown to radio button. Fixed some layout issues. Works well

--- src/App.js
+++ src/App.js
@@ -4,28 +4,21 @@ import InputField from "components/input";
 import IconContainer from "components/IconContainer";
 import { ReactComponent as TypingIcon } from "assets/typing-icon-1.svg";
 import usePersistence from "hooks/usePersistence";
+import DifficultyBar from "utils/difficultyBar";
 
 function App() {
   const [selectValue, setSelectValue] = usePersistence("difficulty", "easy");
   return (
     <div className="App">
       <div className="appContainer">
-        <IconContainer icon={TypingIcon} text="Typing Test" />
-        <div className="menuContainer">
-          <label htmlFor="selectDifficulty">Difficulty</label>
-          <select
-            id="selectDifficulty"
-            value={selectValue}
-            onChange={(e) => {
-              setSelectValue(e.target.value);
-            }}
-            className="menu"
-          >
-            <option value="easy">Easy</option>
-            <option value="moderate">Moderate</option>
-            <option value="hard">Hard</option>
-            <option value="api">API</option>
-          </select>
+        <div className="topBar">
+          <IconContainer icon={TypingIcon} text="Typing Test" />
+          <div className="difficultyContainer">
+            <DifficultyBar
+              initValue={selectValue}
+              setSelectValue={setSelectValue}
+            />
+          </div>
         </div>
 
         <div className="appContent">
---
+++ src/utils/difficultyBar.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+const DifficultyBar = ({ initValue, setSelectValue }) => (
+  <>
+    <div>
+      <input
+        type="radio"
+        name="diff"
+        id="easy"
+        checked={initValue === "easy"}
+        value="easy"
+        onChange={(e) => {
+          setSelectValue(e.target.value);
+        }}
+      />
+      <label htmlFor="easy">Easy</label>
+    </div>
+    <div>
+      <input
+        type="radio"
+        name="diff"
+        id="moderate"
+        checked={initValue === "moderate"}
+        value="moderate"
+        onChange={(e) => {
+          setSelectValue(e.target.value);
+        }}
+      />
+      <label htmlFor="moderate">Moderate</label>
+    </div>
+    <div>
+      <input
+        type="radio"
+        name="diff"
+        id="hard"
+        checked={initValue === "hard"}
+        value="hard"
+        onChange={(e) => {
+          setSelectValue(e.target.value);
+        }}
+      />
+      <label htmlFor="hard">Hard</label>
+    </div>
+    <div>
+      <input
+        type="radio"
+        name="diff"
+        id="api"
+        checked={initValue === "api"}
+        value="api"
+        onChange={(e) => {
+          setSelectValue(e.target.value);
+        }}
+      />
+      <label htmlFor="api">API</label>
+    </div>
+  </>
+);
+
+export default DifficultyBar;
